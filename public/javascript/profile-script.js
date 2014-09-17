8c834617f4507e267fa6f41932d1e7cb6e04ce85
Jerome Pratt <jerome.pratt@outlook.com>
2014-09-17 18:53:10 +0100

    Now a person may only be added once to the page

--- public/javascript/profile-script.js
+++ public/javascript/profile-script.js
@@ -3,16 +3,24 @@ $(document).ready(function(){
 		e.preventDefault();
 	var url = 'https://api.github.com/users/' + $('#username').val() + "?access_token=8bf19aa201701b7d56ee9b4125c9647a39dd766c";
 	var userData = $.get(url, function(user){
-		var template = $('#profile-template').html();
-		$('.profile-container').append(Mustache.render(template, user));
-		}).always(function(){
-			$('#username').val('');
-		}).fail(function(){
-			alert('This github profile does not exist.')
-		});
-		$.get(url, function(user){
-			$('.' + user.login).on('click', function(){
-				$(this).parent().parent().remove();
+			var template = $('#profile-template').html();
+				if($('#' + user.login)[0]){
+					alert('This user is already on the page')
+				}
+				else {
+				$('.profile-container').append(Mustache.render(template, user));
+			}
+			}).always(function(){
+				$('#username').val('');
+			}).fail(function(){
+				alert('This github profile does not exist.')
+			});
+		
+	
+
+			$.get(url, function(user){
+				$('.' + user.login).on('click', function(){
+					$(this).parent().parent().remove();
 			});
 		});
 	});
